6e02ab05aa9f928c61dd490cba2079aae3378edf
flameverk <90734694+flameverk@users.noreply.github.com>
2022-01-05 08:46:33 +0800

    Add files via upload

--- repeater.c
+++ repeater.c
@@ -2,20 +2,13 @@
 
 int main() {
 
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    int d = 0;
-    int e = 0;
+    int input;
 
-    printf("請輸入五個數字，以空白隔開。\n");
-    //add scanf code to get data from input
-    //and feed those input into a, b, c, d, e, respectively.
+    printf("每輸入一個數字，程式會再印出相同的數字。\n");
+    //fill some condition into the following while loop
     //only ONE line code is you need
-    //please refer page 18 of slides
-
-    printf("a=%d\n", a);
-    printf("b=%d\n", b);
-    printf("c=%d\n", c);
-    printf("d=%d\n", d);
-    printf("e=%d\n", e);
+    //please refer page 4 of homework request
+    while (scanf("%d", &input)) {
+        printf("%d\n", input);
+    }
+}
